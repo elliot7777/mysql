aa444d64124cb82cf52c2900ea824a0174b79869
elliot7777 <74612269+elliot7777@users.noreply.github.com>
2022-12-02 13:19:12 -0800

    del.py

---
+++ del.py
@@ -0,0 +1,14 @@
+print("3: teste l'address de boucclage") 
+print("4: voire les taches en cours") 
+print("5: test l'address pupublique de quelqu'un sur Internet") 
+print("6: tester la reponse de mon serveur dns en utilisant leur nom ") 
+print("7: voire IP configuration") print("8 verifier la connectiviter d'un hote qui est deja connecter q notre reseau \n")
+xx = input('\nverifier la: ') 
+if xx == '1':1 
+x = subprocess.getoutput print(x) 
+elif xx == 2: x = subprocess.getouput print(x) 
+elif xx == "3": 
+x = subprocess.getoutput print(x) 
+elif xx == '4': x = subprocess.getoutput print(xx)
+elif xx == '5': 
+('nslookup compagnie.lan') ('ping sv.compagnie.lan -c3') ('ping 127.0.0.1 -c3') ('netstat -an') 
